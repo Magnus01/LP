daf6cfebfdc23ebf17da91e7431c0762b1ad2453
Bruker <magnus01lysfjord@gmail.com>
2017-11-29 09:13:49 +0100

    first

---
+++ js/header.js
@@ -0,0 +1,39 @@
+/*
+ Description: Animates the header upon scroll
+
+ */
+
+$(function(){
+    var header = $("header");
+
+    // $( "header" ).hover(
+    //     function() {
+    //         header.addClass("minimized");
+    //     }, function() {
+    //         header.removeClass("minimized");
+    //     }
+    // );
+    $('#header2').hover(
+        alert('hel')
+        // function(){ header.addClass('minimized') },
+        // function(){ header.removeClass('minimized') }
+    )
+    // var over = false;
+    // $('#header2').hover(function() {
+    //         header.addClass("minimized");
+    //         over = true;
+    //     },
+    //     function () {
+    //         header.removeClass("minimized");
+    //         over = false;
+    //     });
+
+        //
+        //
+        // if( hovered){
+        //     header.addClass("minimized");
+        // }else{
+        //     header.removeClass("minimized");
+        // }
+
+    });
